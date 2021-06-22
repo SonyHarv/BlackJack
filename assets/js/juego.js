9b56a8acf1d9efb8cf8da119bef2b2c13a8ab756
Erikson TF <sonyharv9@gmail.com>
2021-06-22 15:10:41 -0700

    Valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -39,9 +39,21 @@ const pedirCarta = () => {
     }
 
     const carta = deck.pop();
-    console.log(carta);
     console.log(deck);
+    console.log(carta);
     return carta;
 }
 
-pedirCarta();
+// pedirCarta();
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+
+    return isNaN(valor) ?
+        ((valor === 'A') ? 11 : 10) :
+        (valor * 1);
+
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });
